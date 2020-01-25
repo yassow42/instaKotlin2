053d8150c2b74f945f230f2400d617a2b32d5f19
yassow42 <yasinn12@gmail.com>
2020-01-25 22:23:39 +0300

    101 Son

--- app/src/main/java/com/creativeoffice/Home/ChatActivity.kt
+++ app/src/main/java/com/creativeoffice/Home/ChatActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.creativeoffice.instakotlin2.R
+import com.creativeoffice.utils.UniversalImageLoader
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -23,19 +24,24 @@ class ChatActivity : AppCompatActivity() {
         var gelenVeri = intent.extras
         sohbetEdilecekUserID = gelenVeri!!.getString("secilenUserID")!!
 
-        FirebaseDatabase.getInstance().reference.child("users").child(sohbetEdilecekUserID.toString()).child("user_name").addListenerForSingleValueEvent(object : ValueEventListener {
+        FirebaseDatabase.getInstance().reference.child("users").child(sohbetEdilecekUserID).addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onCancelled(p0: DatabaseError) {
             }
+
             override fun onDataChange(p0: DataSnapshot) {
-               var  sohbetEdilecekUserName = p0.value.toString()
+                var sohbetEdilecekUserName = p0.child("user_name").value.toString()
                 tvSohbetEdilecekKullanici.text = sohbetEdilecekUserName
-                Log.e("kişi ", sohbetEdilecekUserName)
+                var profilImg = p0.child("user_detail").child("profile_picture").value.toString()
+                UniversalImageLoader.setImage(profilImg,mesajCircleimg,mesajCirclePB)
+
+
             }
 
         })
 
 
-
-
     }
+
+
+
 }
