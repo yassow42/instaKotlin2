6722d2c10cc367572f7e10eb2b888bfa3d6106b1
yassow42 <yasinn12@gmail.com>
2020-02-04 12:13:39 +0300

    115 bitti

--- app/src/main/java/com/creativeoffice/Home/ChatActivity.kt
+++ app/src/main/java/com/creativeoffice/Home/ChatActivity.kt
@@ -22,6 +22,8 @@ class ChatActivity : AppCompatActivity() {
     lateinit var mRef: DatabaseReference
     var sohbetEdilecekUserId = ""
     var mesajGonderenUserId = ""
+    var konusmaBilgisiGuncellenDimi = false
+
     var tumMesajlar: ArrayList<Mesaj> = ArrayList()
     lateinit var myRecyclerViewAdapter: MesajRecyclerViewAdapter
     lateinit var myRecyclerView: RecyclerView
@@ -55,11 +57,11 @@ class ChatActivity : AppCompatActivity() {
 
         setupMesajlarRecyclerView() //  recyclerViewi olustursun
         mesajlariGetir()
-        refreshLayout.setOnRefreshListener(object : RecyclerRefreshLayout.OnRefreshListener{
+        refreshLayout.setOnRefreshListener(object : RecyclerRefreshLayout.OnRefreshListener {
             override fun onRefresh() {
 
 
-                mRef.child("mesajlar").child(mesajGonderenUserId).child(sohbetEdilecekUserId).addListenerForSingleValueEvent(object : ValueEventListener{
+                mRef.child("mesajlar").child(mesajGonderenUserId).child(sohbetEdilecekUserId).addListenerForSingleValueEvent(object : ValueEventListener {
                     override fun onCancelled(p0: DatabaseError) {
 
                     }
@@ -67,12 +69,12 @@ class ChatActivity : AppCompatActivity() {
                     override fun onDataChange(p0: DataSnapshot) {
 
 
-                        if(p0!!.childrenCount.toInt() != tumMesajlar.size){
-                            dahaFazlaMesajPos=0
+                        if (p0.childrenCount.toInt() != tumMesajlar.size) {
+                            dahaFazlaMesajPos = 0
 
 
                             dahaFazlaMesajGetir()
-                        }else{
+                        } else {
 
                             refreshLayout.setRefreshing(false)
                             refreshLayout.setEnabled(false)
@@ -81,9 +83,6 @@ class ChatActivity : AppCompatActivity() {
 
 
                 })
-
-
-
 
 
             }
@@ -136,12 +135,12 @@ class ChatActivity : AppCompatActivity() {
         }
     }
 
-    private fun dahaFazlaMesajGetir(){
+    private fun dahaFazlaMesajGetir() {
 
 
-        childListenerDahaFazla= mRef.child("mesajlar").child(mesajGonderenUserId).child(sohbetEdilecekUserId)
+        childListenerDahaFazla = mRef.child("mesajlar").child(mesajGonderenUserId).child(sohbetEdilecekUserId)
             .orderByKey().endAt(ilkGetirilenMesajID).limitToLast(SAYFA_BASI_GONDERI_SAYISI)
-            .addChildEventListener(object : ChildEventListener{
+            .addChildEventListener(object : ChildEventListener {
                 override fun onCancelled(p0: DatabaseError) {
 
                 }
@@ -157,28 +156,25 @@ class ChatActivity : AppCompatActivity() {
                 override fun onChildAdded(p0: DataSnapshot, p1: String?) {
 
                     //Log.e("KONTROL","p0 kaç tane veri geldi "+p0!!.childrenCount+" pos:"+dahaFazlaMesajPos)
-                    var okunanMesaj=p0.getValue(Mesaj::class.java)
+                    var okunanMesaj = p0.getValue(Mesaj::class.java)
 
-                    if(!zatenListedeOlanMesajID.equals(p0.key)){
-                        tumMesajlar.add(dahaFazlaMesajPos++,okunanMesaj!!)
-                        myRecyclerViewAdapter.notifyItemInserted(dahaFazlaMesajPos-1)
-                    }else {
+                    if (!zatenListedeOlanMesajID.equals(p0.key)) {
+                        tumMesajlar.add(dahaFazlaMesajPos++, okunanMesaj!!)
+                        myRecyclerViewAdapter.notifyItemInserted(dahaFazlaMesajPos - 1)
+                    } else {
 
-                        zatenListedeOlanMesajID=ilkGetirilenMesajID
+                        zatenListedeOlanMesajID = ilkGetirilenMesajID
 
                     }
 
-                    if(dahaFazlaMesajPos==1){
+                    if (dahaFazlaMesajPos == 1) {
 
-                        ilkGetirilenMesajID= p0.key!!
+                        ilkGetirilenMesajID = p0.key!!
 
                     }
 
 
                     //Log.e("KONTROL","ZATEN LİSTEDEKI ID:"+zatenListedeOlanMesajID+" ILK GETIRILIEN ID:"+ilkGetirilenMesajID+" mesaj id:"+p0!!.key)
-
-
-
 
 
                     myRecyclerView.scrollToPosition(0)
@@ -194,16 +190,13 @@ class ChatActivity : AppCompatActivity() {
             })
 
 
-
-
-
     }
 
     private fun mesajlariGetir() {
 
         tumMesajlar.clear()
 
-        childEventListener= mRef.child("mesajlar").child(mesajGonderenUserId).child(sohbetEdilecekUserId).limitToLast(SAYFA_BASI_GONDERI_SAYISI).addChildEventListener(object : ChildEventListener{
+        childEventListener = mRef.child("mesajlar").child(mesajGonderenUserId).child(sohbetEdilecekUserId).limitToLast(SAYFA_BASI_GONDERI_SAYISI).addChildEventListener(object : ChildEventListener {
             override fun onCancelled(p0: DatabaseError) {
 
             }
@@ -218,21 +211,21 @@ class ChatActivity : AppCompatActivity() {
 
             override fun onChildAdded(p0: DataSnapshot, p1: String?) {
 
-                var okunanMesaj=p0!!.getValue(Mesaj::class.java)
+                var okunanMesaj = p0.getValue(Mesaj::class.java)
                 tumMesajlar.add(okunanMesaj!!)
 
-                if(mesajPos==0){
+                if (mesajPos == 0) {
 
-                    ilkGetirilenMesajID= p0.key!!
-                    zatenListedeOlanMesajID= p0.key!!
+                    ilkGetirilenMesajID = p0.key!!
+                    zatenListedeOlanMesajID = p0.key!!
 
                 }
                 mesajPos++
 
                 mesajGorulduBilgisiniGuncelle(p0.key.toString())
 
-                myRecyclerViewAdapter.notifyItemInserted(tumMesajlar.size-1)
-                myRecyclerView.scrollToPosition(tumMesajlar.size-1)
+                myRecyclerViewAdapter.notifyItemInserted(tumMesajlar.size - 1)
+                myRecyclerView.scrollToPosition(tumMesajlar.size - 1)
 
                 //Log.e("KONTROL","İLK OKUNAN MESAJ ID :"+ilkGetirilenMesajID)
 
@@ -246,15 +239,19 @@ class ChatActivity : AppCompatActivity() {
         })
 
 
-
     }
 
-     fun mesajGorulduBilgisiniGuncelle(mesajID: String) {
+    fun mesajGorulduBilgisiniGuncelle(mesajID: String) {
         FirebaseDatabase.getInstance().reference.child("mesajlar")
             .child(mesajGonderenUserId)
             .child(sohbetEdilecekUserId).child(mesajID)
             .child("goruldu").setValue(true)
+            .addOnCompleteListener {
+                FirebaseDatabase.getInstance().reference.child("konusmalar").child(mesajGonderenUserId).child(sohbetEdilecekUserId).child("goruldu").setValue(true)
+            }
     }
+
+
 
     private fun setupMesajlarRecyclerView() {
         var myLinearLayoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
@@ -297,7 +294,15 @@ class ChatActivity : AppCompatActivity() {
 
     }
 
+    override fun onStart() {
+        super.onStart()
+    }
 
+    override fun onStop() {
 
+        FirebaseDatabase.getInstance().reference.child("mesajlar").child(mesajGonderenUserId).child(sohbetEdilecekUserId).removeEventListener(childEventListener)
+        super.onStop()
+    }
 
 }
+
